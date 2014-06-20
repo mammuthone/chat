2d0d0f686ace405d989fb72f847aa370f8ec3973
mammuthone <perconte@hotmail.it>
2014-06-20 02:59:24 +0200

    one page redesigned chat

---
+++ README.md
@@ -0,0 +1,5 @@
+chat
+====
+
+A simple chat with nodejs, expressjs, jquery and bootstrap
+
---
+++ public/js/jscript.js
@@ -0,0 +1,48 @@
+jQuery(function($){
+		var socket = io.connect(),
+				nickForm = $('#setNick'),
+				nickError	= $('#nickError'),
+				nickBox = $('#nickname'),
+				confirmed = $('#confirmedNick'),
+				users = $('#users'),
+				messageForm = $('#send-message'),
+				messageBox = $('#message'),
+				chat = $('#chat ul');
+		
+		nickForm.on('submit',function(e){
+			e.preventDefault();
+			var u = nickBox.val();
+			if( u.length == 0 ){
+				alert("Error! You must insert a valid nickname");
+				return false;
+			}
+			socket.emit('new user', u, function(data){
+				if(data) {
+					$('#setNick').hide();
+					$('#jumbo .toHide').hide();
+					confirmed.css('display','inline').html("you're chatting with <b>" + u + "</b> username");
+				} else {
+					nickError.html('That username is already taken! Try again');
+				}
+			});
+		});
+		
+		socket.on('usernames', function(data){
+			var html = '',
+					icon = '<span class="glyphicon glyphicon-user"> '
+			for ( i=0; i< data.length; i++){
+					html += icon + data[i] + '</span><br/>'
+			}
+			users.html(html);
+		});
+		
+		messageForm.submit(function(e){
+			e.preventDefault();
+			socket.emit('send message', messageBox.val());
+			messageBox.val('');
+		});
+
+		socket.on('new message', function(data){
+			chat.append('<li><b>'+ data.nick +': </b>' + data.msg + "</li>");
+		});
+	});
